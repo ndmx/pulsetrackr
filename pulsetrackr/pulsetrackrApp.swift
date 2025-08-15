05112372cf330489ef2f1bcbe8624d655cb5fa04
ndmx <alexanderukaga@gmail.com>
2025-08-14 21:36:43 -0400

    Initial Commit

---
+++ pulsetrackr/pulsetrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulsetrackrApp.swift
+//  pulsetrackr
+//
+//  Created by Alexander Ukaga on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct pulsetrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulsetrackrTests/pulsetrackrTests.swift
@@ -0,0 +1,17 @@
+//
+//  pulsetrackrTests.swift
+//  pulsetrackrTests
+//
+//  Created by Alexander Ukaga on 8/14/25.
+//
+
+import Testing
+@testable import pulsetrackr
+
+struct pulsetrackrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
